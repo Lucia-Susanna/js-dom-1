fd841229e25f8fe006f0e6b4eae35eb5675582a5
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-11-21 19:24:25 +0100

    feat: bg-color changing

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const lampadinaSpenta = document.getElementById('spenta')
 const lampadinaAccesa = document.getElementById('accesa')
 const interruttore = document.getElementById('interruttoreLampadina')
+const coloreSfondo = document.querySelector('.sfondo')
 
 interruttore.addEventListener('click', ()=>{
   lampadinaAccesa.classList.toggle('none')
+  coloreSfondo.classList.toggle('dark')
+  coloreSfondo.classList.toggle('light')
   interruttore.innerHTML = lampadinaAccesa.className.includes('none') ? 'Accendi'
-                                                                    : 'Spegni'
+                                                                     : 'Spegni'
 })
 
